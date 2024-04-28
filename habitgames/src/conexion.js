ad602610e824cf4a0faef21e7ba88f30292413ee
Venegas86 <vp7venegas86@gmail.com>
2024-04-28 16:38:28 +0200

    remplazo

---
+++ habitgames/src/conexion.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+// MySQL Connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Raxila2021as',
+    database: 'habit_games',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+
+
+module.exports = connection;
